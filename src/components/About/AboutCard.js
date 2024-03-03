4738e9ef6b19514bb8afc27af2c5d933040fbcb4
Syafiq Lim <38598679+Syafiqlim@users.noreply.github.com>
2024-03-04 00:00:38 +0800

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -14,14 +14,35 @@ function AboutCard() {
             I am currently a student at Politenik Merlimau, enrolling in Diploma Electronic (Computer) Engineering programme.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            Achievements and Certfications (Certs and badges verifications are available on LinkedIn)
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Reading books and articles
+              <ImPointRight /> Arcitura Certified IoT Architect (2023)
             </li>
             <li className="about-activity">
-              <ImPointRight /> Learning new stuff
+              <ImPointRight /> Postman API Fundamentals Student Expert certification (2024)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> WorldSkills Malaysia Belia Electronics Participant Certificate (2023)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Coursera Google Cybersecurity Professional Certificate (Ongoing)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Google Cloud Skill Badges (2022-2023)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Codered Certificates of Achievements (Cyber Security Courses) (2022)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Google Developer Skill Badges (2023)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Chairman of JKE PMM Club (2024)
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Malaysia Future Leaders School (MFLS) Tier 2 Certificate (2019)
             </li>
           </ul>
         </blockquote>
