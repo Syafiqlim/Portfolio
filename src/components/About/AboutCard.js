91689db6610e1b7ef9159bf02e28c9fff174127b
Syafiqlim <syafiqmuhammad588@gmail.com>
2024-06-10 23:51:24 +0800

    modified:   src/components/About/AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,7 +7,7 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <h4 style={{ textAlign: "justify" }}>
+          <h4 style={{ textAlign: "left" }}>
             🧍 Muhammad Syafiq Bin Muhammad Syafee Lim
             <br />
             🏠 Melaka, Malaysia
@@ -17,32 +17,35 @@ function AboutCard() {
             👨‍💻Engineer Intern (IoT & Embedded System Engineering) at iRadar Sdn Bhd (<a href="https://iradar.com.my/">iradar.com.my</a>)
             <br />
             <br />
+            <br />
             </h4>
-            <h3 style={{textAlign: "justify"}}> 
+            <h3 style={{textAlign: "left", fontWeight:'bold'}}> 
                 Skills, Languages, Tools
           </h3>
           <ul>
             <li className="about-activity">
-              <h4>Programming Languages : </h4>
+              <h4 style={{fontWeight:'bold'}}>Programming Languages : </h4>
               <ImPointRight /> C/C++ (including C Preprocessor), Embedded C (Arduino-based, STM32-based, PIC18/32), JavaScript, TypeScript, Python, SQL, Bash Scripting
             </li>
+            <br />
             <li className="about-activity">
-              <h4>IoT, Embedded & Electronic Skills : </h4>
+              <h4 style={{fontWeight:'bold'}}>IoT, Embedded & Electronic Skills : </h4>
               <ImPointRight /> uC : Arduino-based uC, ESP32/8266, STM32-based uC, Raspberry Pi <br/>
               <ImPointRight /> Interfaces & Protocols : UART, I2C, SPI, BLE, WiFi, TCP/IP, UDP, HTTP/HTTPS, MQTT, GSM/LTE, 5G, LoRa, CoAP, Modbus, SSH <br/>
               <ImPointRight /> RTOS : FreeRTOS <br/>
               <ImPointRight /> Debugging : JTAG, OpenOCD <br/>
               <ImPointRight /> Electronic Circuits Design, PCB Design, THT & SMT Soldering, testing & troubleshooting (multimeter, oscilloscope)
             </li>
+            <br />
             <li className="about-activity">
-              <h4>Web Development Skills</h4>
+              <h4 style={{fontWeight:'bold'}}>Web Development Skills</h4>
               <ImPointRight /> Front-End : HTML, CSS, JavaScript, React, Vite <br />
               <ImPointRight /> Back-End : Node.js (JavaScript, TypeScript), Express.js, MySQL
             </li>
           </ul>
             <br />
             <br />
-          <p style={{textAlign: "justify"}}> 
+          <p style={{textAlign: "left"}}> 
                 Achievements and Certfications (Certs and badges verifications are available on LinkedIn)
           </p>
           <ul>
